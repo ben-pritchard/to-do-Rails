877fa30b11a083dc4c2e20237525f58ea552ae73
Ben <ben.s.pritchard@gmail.com>
2015-03-16 12:01:19 -0700

    Add ability to delete lists and show flash notices for successful updates. Also add paritals for form editing and errors display.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,22 +1,20 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
-    render :index
   end
 
   def show
     @list = List.find(params[:id])
-    render :show
   end
 
   def new
     @list = List.new
-    render :new
   end
 
   def create
     @list = List.new(params[:list])
     if @list.save
+      flash[:notice] = "List successfully added!"
       redirect_to lists_path
     else
       render :new
@@ -25,16 +23,23 @@ class ListsController < ApplicationController
 
   def edit
     @list = List.find(params[:id])
-    render :edit
   end
 
   def update
     @list = List.find(params[:id])
     if @list.update(params[:list])
+      flash[:notice] = "List successfully updated!"
       redirect_to lists_path
     else
       render :edit
     end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    flash[:notice] = "List successfully destroyed!"
+    redirect_to lists_path
   end
 
 end
